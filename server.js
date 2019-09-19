e2d2b898a77fd9e064d264d1d10c5b2b18a4fe33
Paul cartau <pcartau@student.42.fr>
2019-09-19 10:16:38 +0200

    yt-mp3-dl for faster

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const bodyParser = require('body-parser');
 const ytdl = require('ytdl-core');
-const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
+const YoutubeMp3Downloader = require("youtube-mp3-downloader");
+
+const YD = new YoutubeMp3Downloader({
+  "ffmpegPath": "./vendor/ffmpeg/ffmpeg",        // Where is the FFmpeg binary located?
+  "outputPath": "./",                     // Where should the downloaded and encoded files be stored?
+  "youtubeVideoQuality": "highest",       // What video quality should be used?
+  "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
+  "progressTimeout": 2000                 // How long should be the interval of the progress reports
+});
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,14 +18,9 @@ app.use(bodyParser.json());
 
 app.get('/getVideo-*', (req, res) => {
   ytdl.getInfo(req.url.substring(10), (err, info) => {
-    const stream = ytdl(req.url.substring(10), {
-      quality: 'highestaudio',
-    })
-    proc = new ffmpeg({source:stream})
-    proc.setFfmpegPath('./vendor/ffmpeg/ffmpeg');
-    proc.saveToFile('./output.mp3')
-    .on('end', () => {
-      res.download('./output.mp3', `./${info.title}.mp3`);
+    YD.download(req.url.split('=')[1], 'output.mp3');
+    YD.on("finished", () => {
+      res.download('./output.mp3', `${info.title}.mp3`);
     })
   });
 });
