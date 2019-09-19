369ac380e2ce4f4bf21ae8ec8dcee6676ed4db42
Paul cartau <pcartau@student.42.fr>
2019-09-19 10:26:07 +0200

    patch mobile url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ytdl = require('ytdl-core');
 const YoutubeMp3Downloader = require("youtube-mp3-downloader");
 
 const YD = new YoutubeMp3Downloader({
-  "ffmpegPath": "./vendor/ffmpeg/ffmpeg",        // Where is the FFmpeg binary located?
+  "ffmpegPath": "./FFmpeg/ffmpeg",        // Where is the FFmpeg binary located?
   "outputPath": "./",                     // Where should the downloaded and encoded files be stored?
   "youtubeVideoQuality": "highest",       // What video quality should be used?
   "queueParallelism": 2,                  // How many parallel downloads/encodes should be started?
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.get('/getVideo-*', (req, res) => {
   ytdl.getInfo(req.url.substring(10), (err, info) => {
-    YD.download(req.url.split('=')[1], 'output.mp3');
+    YD.download(req.url.split('=')[1] || req.url.split('/')[4], 'output.mp3');
     YD.on("finished", () => {
       res.download('./output.mp3', `${info.title}.mp3`);
     })
